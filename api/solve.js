8ade9905e08cef35afcffacc60b84ce3048c2953
solvemaths206-afk <solvemaths206@gmail.com>
2026-08-04 21:55:41 +0500

    Update solve.js

--- api/solve.js
+++ api/solve.js
@@ -11,7 +11,7 @@ if (req.method === "OPTIONS") {
   }
 
   try {
-    const { question, image, mode, grade, subject } = req.body;
+  const { question, image, mode, grade, subject, studentName } = req.body;  
 console.log("Subject:", subject);
 console.log("Grade:", grade);
    let prompt = "";
