7849b36f3241200d18f6c84f6c002651be3e95d5
solvemaths206-afk <solvemaths206@gmail.com>
2026-08-04 05:40:03 +0500

    Update solve.js

--- api/solve.js
+++ api/solve.js
@@ -7,7 +7,8 @@ module.exports = async function handler(req, res) {
     const { question } = req.body;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`
+  `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  {
       {
         method: "POST",
         headers: {
